12921911a1bcb09b1622a066504cea9eb7eba669
Kunalsaini1303 <140806752+Kunalsaini1303@users.noreply.github.com>
2024-01-29 02:02:23 +0000

    conversion of data type

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,27 @@
+let score = "kunal"
+
+console.log(typeof score );
+console.log(typeof(score)); //written as method in brkts
+
+let valueinnumber= Number  (score)
+console.log(typeof valueinnumber);
+console.log(valueinnumber); 
+
+// "33" => 33
+// "33abc" => NaN (not a number)
+// true => 1; false => 0
+
+let isloggedin = "kunal"
+
+let booleanisloggedin = Boolean (isloggedin)
+console.log(booleanisloggedin);
+
+// 1 converted in true in boolean 
+// 0 converted in false in boolean 
+// " " empty string converted in false
+// "kunal" converted in true 
+
+let somenumber = 33
+let stringnumber = String (somenumber)
+console.log(stringnumber);
+console.log(typeof (stringnumber));
