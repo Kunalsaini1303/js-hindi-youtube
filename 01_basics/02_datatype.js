94967d85c543dbf21cf5ea148857f3941fc57f2f
Kunalsaini1303 <140806752+Kunalsaini1303@users.noreply.github.com>
2024-01-29 01:29:39 +0000

    data type

---
+++ 01_basics/02_datatype.js
@@ -0,0 +1,23 @@
+"use strict"; //treat all js code as newwe version
+
+//ale rt (3+3) // we are using node js, not browser
+
+console.log(3+3); 
+console.log("kunal")
+
+let name ="kunal "
+let age = 20
+let isloggedid = false 
+ 
+// number => 2 to power 53 
+// bigInt 
+// string => ""
+// boolean => true/false
+// null =>stanlone value
+// undefined => variable assigned but value not assigned
+//symbol => unique  
+
+//object 
+
+console.log(typeof null); //op is object as null is a object
+console.log(typeof undefined); //op is undefined as undefined is a data type
